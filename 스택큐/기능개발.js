a031155fc204ced5a854312b9fa427179f3e5b4e
ucom96 <57552154+ucom96@users.noreply.github.com>
2022-02-09 18:01:50 +0900

    update 기능개발.js
    -코드 실행 통과

--- 스택큐/기능개발.js
+++ 스택큐/기능개발.js
@@ -2,7 +2,10 @@ function solution(progresses, speeds) {
   let answer = [0];
 
   //배포까지 걸리는 기간
-  let first = 100; //기간끼리의 비교를 하기 위해 설정
+  let first = (100 - progresses[0]) / speeds[0];
+  if ((100 - progresses[0]) % speeds[0] !== 0) {
+    first = Math.floor(first) + 1;
+  }
 
   //answer의 idx
   let k = 0;
@@ -10,17 +13,17 @@ function solution(progresses, speeds) {
   for (let i = 0; i < progresses.length; i++) {
     let next = (100 - progresses[i]) / speeds[i];
     if ((100 - progresses[i]) % speeds[i] !== 0) {
-      next++;
+      next = Math.floor(next) + 1;
     }
+    console.log("first", first);
+    console.log("next", next);
     //배포기간 비교
     if (first >= next) {
-      answer[k]++;
+      answer[k] += 1;
     } else {
-      answer.push(1);
-      k++;
+      answer[++k] = 1;
+      first = next;
     }
-
-    first = next;
   }
 
   return answer;

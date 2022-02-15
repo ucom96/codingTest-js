13062af2795b2a78f821f310337d79d29d16eae0
ucom96 <57552154+ucom96@users.noreply.github.com>
2022-02-15 17:21:28 +0900

    Update 큰수만들기ans.js

--- 그리디/큰수만들기ans.js
+++ 그리디/큰수만들기ans.js
@@ -1,5 +1,5 @@
 function solution(number, k) {
-  //큰 수만 넣어두는 스택
+  //number의 수와 비교하여 더 큰수를 넣어둘 스택
   let stack = [];
   //number의 인덱스를 표시해주는 변수
   let head = 0;
@@ -9,16 +9,16 @@ function solution(number, k) {
   //스택에 삽입, 삭제를 반복적으로 실행한다
   while (stack.length < number.length - k || head < number.length) {
     //스택에 있는 수와 number의 수를 비교한다
-    //스택에 있는 수가 number의 수보다 크거나 같다면
+    //스택에 있는 수가 number의 수보다 작다면
     if (stack[stack.length - 1] < number[head] && cnt) {
-      //스택에 있는 수를 빼주고 number에 있는 수를 스택에 넣는다
+      //스택에 있는 수를 빼주고 숫자를 하나 제거했으므로 cnt를 하나 감소시킨다
       stack.pop();
       cnt--;
+      //head를 증가시키지 않은 상태에서 number[head]와 스택에 남아있는 수들을 반복적으로 비교한다
       continue;
     }
     stack.push(number[head++]);
   }
-
   return stack.slice(0, number.length - k).join("");
 }
 

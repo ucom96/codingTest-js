850afae618b284a3f9cd200d4fda0715d5580b0b
ucom96 <57552154+ucom96@users.noreply.github.com>
2022-02-16 21:44:05 +0900

    Update 가장큰수ans.js

--- 정렬/가장큰수ans.js
+++ 정렬/가장큰수ans.js
@@ -1,1 +1,12 @@
+function solution(numbers) {
+  var answer = "";
+  //numbers의 숫자요소들을 문자열로 바꿈
+  let strings = numbers.map((number) => number + "");
+  //strings의 요소들을 앞뒤로 묶어서 내림차순으로 정렬후 전체를 문자열로 만들기
+  answer = strings.sort((a, b) => b + a - (a + b)).join("");
 
+  return answer[0] === "0" ? "0" : answer;
+}
+
+const numbers = [0, 0, 0, 0];
+console.log(solution(numbers));
